3b636f2fbde3acc357f9068ee7c93fcc0ac6a676
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-12-17 14:47:45 +0900

    Nextjs (#2)
    * prettierを追加しフォーマット
    
    * 古いファイルをすべて移動
    
    * next.js init
    
    * metadataの簡易的な編集
    
    * ファビコンの変更
    
    * ヘッダーの作成
    
    * 効率化
    
    * フッターの作成
    
    * LPをあらかた作成
    
    * 最近の記事リストを追加し、LPを完成
    
    * スタイルの調整
    
    * 本文ページの作成
    
    * 工事中メッセージの追加
    
    * リンクを追加
    
    * 古いファイル群を削除
    
    * CICD用にセットアップ

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+const SiteSetting = {
+  SocialLinks: {
+    Discord: "https://discord.com/invite/Nus2xg3nF3",
+    GitHub: "https://github.com/Rust-Developers-JP",
+  },
+};
+
+export default SiteSetting;
